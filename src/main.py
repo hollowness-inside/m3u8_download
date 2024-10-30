94c260626156c1cf343f50ca07e6291e79d63e2f
hollowness-inside <malikov.vp@students.dvfu.ru>
2024-10-30 21:17:59 +1000

    Implement download_segment

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import asyncio
 import pickle
-from os import path
+from os import path, makedirs
 
 import aiohttp
 
 
 VERBOSE = True
 SEGMENTS_CACHE = "parsed_segments.pickle"
+SEGMENTS_DIR = "segments"
+
+makedirs(SEGMENTS_DIR, exist_ok=True)
 
 if VERBOSE:
     vprint = lambda *args: print(*args)
@@ -14,7 +17,7 @@ else:
     vprint = lambda *_: None
 
 
-async def download_m3u8(session: aiohttp.ClientSession, url, force_ext: str | None = None, cache: bool = False):
+async def download_m3u8(session: aiohttp.ClientSession, url: str, force_ext: str | None = None, cache: bool = False):
     vprint("Downloading .m3u8")
 
     if cache and path.exists("segments.m3u8"):
@@ -60,8 +63,27 @@ def parse_m3u8(m3u8_data: str, force_ext: str | None = None):
                 "filename": key,
                 "url": url
             })
-            
+
     return segments
+
+
+async def download_segment(session: aiohttp.ClientSession, segment: dict[str, str]):
+    fname = segment['filename']
+    url = segment['url']
+
+    try:
+        async with session.get(url) as response:
+            response.raise_for_status()
+
+            content = await response.read()
+
+            fout = path.join(SEGMENTS_DIR, fname)
+            with open(fout, 'wb') as f:
+                f.write(content)
+
+            vprint(f"Downloaded segment {fname}")
+    except Exception as e:
+        vprint(f"Failed to download segment {fname}: {e}")
 
 
 HEADERS = {
@@ -76,5 +98,7 @@ async def main():
         url = 'some_url.m3u8'
         segments = await download_m3u8(session, url, force_ext='.ts')
         print(segments)
+
+        await download_segment(session, segments[0])
 
 asyncio.run(main())
